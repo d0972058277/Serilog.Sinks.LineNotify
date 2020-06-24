c3c5008ef17db9d00349854afb27bf5c0eb88312
御霖 <gn02053231@gmail.com>
2020-06-24 15:17:48 +0800

    init

---
+++ src/LineNotifyLoggerConfigurationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Sinks.LineNotify;
+using Serilog.Sinks.PeriodicBatching;
+using System;
+using System.Collections.Generic;
+
+namespace Serilog
+{
+    public static class LineNotifyLoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration LineNotify(
+            this LoggerSinkConfiguration loggerSinkConfiguration,
+            string outputTemplate,
+            IEnumerable<string> lineNotifyTokens,
+            IFormatProvider formatProvider = null,
+            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose)
+        {
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+
+            var sink = new LineNotifySink(formatter, lineNotifyTokens);
+
+            var batchingSink = new PeriodicBatchingSink(sink, new PeriodicBatchingSinkOptions
+            {
+                BatchSizeLimit = 1,
+                Period = TimeSpan.FromSeconds(1),
+                QueueLimit = 1000
+            });
+
+            return loggerSinkConfiguration.Sink(batchingSink, restrictedToMinimumLevel);
+        }
+    }
+}
---
+++ src/Sinks/LineNotify/LineNotifySink.cs
@@ -0,0 +1,98 @@
+﻿using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.PeriodicBatching;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.LineNotify
+{
+    class LineNotifySink : IBatchedLogEventSink
+    {
+        private const int DefaultWriteBufferCapacity = 256;
+
+        private readonly HttpClient _httpClient;
+
+        private readonly string _lineNotifyApiUrl;
+        private readonly ITextFormatter _textFormatter;
+        private readonly IEnumerable<string> _lineNotifyTokens;
+
+        public LineNotifySink(ITextFormatter textFormatter, IEnumerable<string> lineNotifyTokens, string lineNotifyApiUrl = "https://notify-api.line.me/api/notify")
+        {
+            if (!lineNotifyTokens.Any())
+            {
+                throw new ArgumentException("長度不能為0。", nameof(lineNotifyTokens));
+            }
+
+            _httpClient = new HttpClient();
+            _lineNotifyApiUrl = lineNotifyApiUrl;
+            _textFormatter = textFormatter;
+            _lineNotifyTokens = lineNotifyTokens;
+        }
+
+        public async Task EmitBatchAsync(IEnumerable<LogEvent> batch)
+        {
+            foreach (var logEvent in batch)
+            {
+                var message = FormatMessage(logEvent);
+
+                foreach (var lineNotifyToken in _lineNotifyTokens)
+                {
+                    await NotifyAsync(lineNotifyToken, message);
+                }
+            }
+        }
+
+        public Task OnEmptyBatchAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        private string FormatMessage(LogEvent logEvent)
+        {
+            var buffer = new StringWriter(new StringBuilder(DefaultWriteBufferCapacity));
+
+            _textFormatter.Format(logEvent, buffer);
+
+            var formatMessage = buffer.ToString();
+
+            return formatMessage;
+        }
+
+        private async Task NotifyAsync(string token, string message)
+        {
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, _lineNotifyApiUrl))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                requestMessage.Content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("message",  message)
+                });
+
+                var response = await _httpClient.SendAsync(requestMessage);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new LoggingFailedException($"此Token已失效 : {token}");
+                }
+                else
+                {
+                    var body = response.Content.ReadAsStringAsync();
+
+                    throw new LoggingFailedException($"發送Line Notify時發生錯誤，HttpStatusCode : {response.StatusCode}，body: {body}");
+                }
+            }
+        }
+    }
+}
